288cf77847374d801bdd551d4cd380756d077804
Denise Aguirre M <denny.aguirre01@gmail.com>
2025-04-07 15:46:44 -0300

    add test tasks controller

--- src/tasks/dto/create-task.dto.ts
+++ src/tasks/dto/create-task.dto.ts
@@ -1,5 +1,13 @@
+import { IsString, IsBoolean, IsOptional } from 'class-validator';
+
 export class CreateTaskDto {
-  title: string;
-  description?: string;
+  @IsString()
+  title!: string;
+
+  @IsString()
+  description!: string;
+
+  @IsBoolean()
+  @IsOptional()
   completed?: boolean;
 }
